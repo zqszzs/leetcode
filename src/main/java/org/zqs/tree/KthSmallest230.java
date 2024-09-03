2cbc6496a6faa93e3f3e8b6848ec8b0654fd269f
zhengqingshun <qingshun.zheng@cimc.com>
2024-09-03 10:23:49 +0800

    20230903: 二叉搜索树中的第K个小元素

--- src/main/java/org/zqs/tree/KthSmallest230.java
+++ src/main/java/org/zqs/tree/KthSmallest230.java
@@ -2,11 +2,32 @@ package org.zqs.tree;
 
 import org.zqs.common.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 二叉搜索树中的第K个小元素
+ */
 public class KthSmallest230 {
 
     public int kthSmallest(TreeNode root, int k) {
+        int i = 1;
+        TreeNode leftNode = root;
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (!stack.isEmpty() || root != null) {
+            while (leftNode != null) {
+                stack.push(leftNode);
+                leftNode = leftNode.left;
+            }
 
+            TreeNode treeNode = stack.pop();
+            if (i == k) {
+                return treeNode.val;
+            }
+            leftNode = treeNode.right;
+            i++;
+        }
 
-        return 0;
+        return -1;
     }
 }
